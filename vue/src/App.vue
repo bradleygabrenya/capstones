faa92bdbfd96b6fcd049df1c355149a831e37d35
Yevgenia Samolyuk <yevgsamolyuk@gmail.com>
2022-04-13 14:28:05 -0400

    Added nav option for admins

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }">Home </router-link>
       
-      <router-link v-bind:to="{ name: 'workouts', params: {userId: this.$store.state.user.userId}}">Past Workouts</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      
+      <router-link v-bind:to="{ name: 'workouts', params: {userId: this.$store.state.user.userId}}" v-if="$store.state.token != ''">Past Workouts </router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout </router-link>
+      <router-link v-bind:to="{name: 'user-list'}" v-if="this.$store.state.user.role === 'admin' || this.$store.state.user.role==='employee'">View Members </router-link>
     </div>
     <router-view />
   </div>
